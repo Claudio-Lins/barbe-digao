23d3b7251bf51eac016aceea13177a030feb573c
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2022-03-01 20:10:01 +0000

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 
-import { GlobalStyle } from './styles/global';
+import './styles/index.css';
+// import { GlobalStyle } from './styles/global';
 
 export function App() {
   return( 
   <div className="App">
-    <h1>Barbe Digão</h1>
+    <h1 className="text-red-600">Barbe Digão</h1>
+    {/* <GlobalStyle /> */}
   </div>);
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,9 @@
+
+
+
 import { createGlobalStyle } from 'styled-components';
+
+
 
 export const GlobalStyle = createGlobalStyle`
   :root {
